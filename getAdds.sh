a4d90d1f4dd7c2261b67c82499de9c4d95608075
jkiesele <>
2012-08-09 14:57:03 +0000

    *** empty log message ***

--- getAdds.sh
+++ getAdds.sh
@@ -4,4 +4,11 @@ cvs co -d TopAnalysis/TopFilter/plugins/ UserCode/Bromo/TopAnalysis/TopFilter/pl
 cvs co -d TopAnalysis/TopFilter/plugins/ UserCode/Bromo/TopAnalysis/TopFilter/plugins/GeneratorTopFilter.cc
 cvs co -d TopAnalysis/TopFilter/python/filters/ UserCode/Bromo/TopAnalysis/TopFilter/python/filters/GeneratorTopFilter_cfi.py
 cvs co -d TopAnalysis/TopFilter/python/filters/ UserCode/Bromo/TopAnalysis/TopFilter/python/filters/GeneratorZFilter_cfi.py
-addpkg CondFormats/JetMETObjects
+
+cp -r /afs/naf.desy.de/user/k/kieseler/public/code/CondFormats .
+
+cvs co -r V00-00-13 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
+cd EGamma/EGammaAnalysisTools/data
+cat download.url | xargs wget
+cd $CMSSW_BASE
+cd src
