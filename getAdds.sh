4ef05549bd7c31f1194cdb42f2367104e4a65363
jkiesele <>
2012-08-10 14:44:07 +0000

    added some info

--- getAdds.sh
+++ getAdds.sh
@@ -5,10 +5,22 @@ cvs co -d TopAnalysis/TopFilter/plugins/ UserCode/Bromo/TopAnalysis/TopFilter/pl
 cvs co -d TopAnalysis/TopFilter/python/filters/ UserCode/Bromo/TopAnalysis/TopFilter/python/filters/GeneratorTopFilter_cfi.py
 cvs co -d TopAnalysis/TopFilter/python/filters/ UserCode/Bromo/TopAnalysis/TopFilter/python/filters/GeneratorZFilter_cfi.py
 
-cp -r /afs/naf.desy.de/user/k/kieseler/public/code/CondFormats .
+cp -r /afs/desy.de/user/k/kiesej/public/code/CondFormats .
 
 cvs co -r V00-00-13 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
 cd EGamma/EGammaAnalysisTools/data
 cat download.url | xargs wget
-cd $CMSSW_BASE
-cd src
+#cd ../../../
+echo 'you will need this in the rootlogon.C:'
+echo '
+     gSystem->Load("libFWCoreFWLite.so"); 
+     AutoLibraryLoader::enable();
+     gSystem->Load("libDataFormatsFWLite.so");
+     gSystem->Load("libDataFormatsPatCandidates.so");
+     gSystem->Load("libTtZAnalysisDataFormats.so");
+     gSystem->Load("libRooFit");
+     gSystem->Load("libFWCoreUtilities.so");
+     gSystem->Load("libCondFormatsJetMETObjects.so");
+     cout << "libs loaded" <<endl;
+     '
+echo 'dont forget to checkout the latest pat release'
