c7378f020013641af9f9aaae304bb0e0e680897e
jkiesele <>
2012-10-05 14:31:49 +0000

    added ecaldrivenmomentum to electrons

--- TreeWriter/src/TreeWriter.cc
+++ TreeWriter/src/TreeWriter.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jan Kieseler,,,DESY
 //         Created:  Fri May 11 14:22:43 CEST 2012
-// $Id: TreeWriter.cc,v 1.8 2012/08/28 09:56:54 jkiesele Exp $
+// $Id: TreeWriter.cc,v 1.9 2012/10/05 13:13:11 jkiesele Exp $
 //
 //
 
@@ -299,7 +299,7 @@ TreeWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	 
 
        top::NTElectron tempelec;
-       // if(electron->ecalDrivenMomentum())
+       if(electron->ecalDrivenMomentum())
        //   tempelec.setECalP4(electron->ecalDrivenMomentum());
        
        tempelec.setP4(electron->p4());
@@ -369,7 +369,7 @@ TreeWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
        top::NTElectron tempelec;
        //  if(electron->ecalDrivenMomentum())
-       //   tempelec.setECalP4(electron->ecalDrivenMomentum());
+       tempelec.setECalP4(electron->ecalDrivenMomentum());
        
        tempelec.setP4(electron->p4());
        double vz=-9999;
