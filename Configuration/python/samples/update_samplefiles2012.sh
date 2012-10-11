a12e1ee77f11060bd8ea6561657695868d8228db
jkiesele <>
2012-10-11 09:31:56 +0000

    changed naming scheme to match diffxsec

--- Configuration/python/samples/update_samplefiles2012.sh
+++ Configuration/python/samples/update_samplefiles2012.sh
@@ -13,8 +13,11 @@ filenames=('DoubleElectron/Run2012A-13Jul2012-v1'
 'DoubleMu/Run2012C-24Aug2012-v1'
 'MuEG/Run2012C-24Aug2012-v1'
 'SingleMu/Run2012A-13Jul2012-v1'
+'SingleMu/Run2012A-recover-06Aug2012-v1'
 'SingleElectron/Run2012A-13Jul2012-v1'
+'SingleElectron/Run2012A-recover-06Aug2012-v1'
 'MET/Run2012A-13Jul2012-v1'
+'MET/Run2012A-recover-06Aug2012-v1'
 'SingleMu/Run2012B-13Jul2012-v1'
 'SingleElectron/Run2012B-13Jul2012-v1'
 'MET/Run2012B-13Jul2012-v1'
@@ -34,6 +37,7 @@ for (( i=0;i<$entries;i++)); do
     address=$pre${filenames[${i}]}$post
     # savename=`echo ${filenames[${i}]} | awk '{split($0,a,"/"); print a[1]}'`
     savename=`echo ${filenames[${i}]} | sed 's/\//_/g'`   #too long
+    savename=`echo ${filenames[${i}]} | sed 's/-/_/g'`   #too long
     wget $address -O ${savename}_cff.py --no-check-certificate
   #  echo $address
     echo $savename
