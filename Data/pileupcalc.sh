7317d515676b2db2df30b602f7fb8257c3b5d2fc
jkiesele <>
2012-11-28 14:22:08 +0000

    *** empty log message ***

--- Data/pileupcalc.sh
+++ Data/pileupcalc.sh
@@ -5,15 +5,15 @@ analysisJson=$2
 
 #default
 
-pileupCalc.py --minBiasXsec 69400 --inputLumiJSON $pileupJson --maxPileupBin 60 --numPileupBins 60 --calcMode true -i $analysisJson ${analysisJson}_PU.root
+pileupCalc.py --minBiasXsec 69400 --inputLumiJSON $pileupJson --maxPileupBin 50 --numPileupBins 50 --calcMode true -i $analysisJson ${analysisJson}_PU.root
 
 #up
 
-pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 60 --numPileupBins 60 --calcMode true -i $analysisJson ${analysisJson}_PU_up.root
+#pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${analysisJson}_PU_up.root
 
 #down
 
-pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 60 --numPileupBins 60 --calcMode true -i $analysisJson ${analysisJson}_PU_down.root
+#pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${analysisJson}_PU_down.root
 
 #up: 72870
 #down 65930
