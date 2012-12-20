af7ff7ff068ecd617a84791d680a7c095b58f972
jkiesele <>
2012-12-20 12:57:36 +0000

    added 2d stuff here

--- Analysis/triggerEfficiencies12/makeplotsnice.h
+++ Analysis/triggerEfficiencies12/makeplotsnice.h
@@ -12,148 +12,183 @@
 
 #include "TH2D.h"
 
-
-void makeplot(TString inputfile, TString add){
-
-  TLegend * leg =  new TLegend(0.35,0.15,0.70,0.42);
-  leg->Clear();
-  leg->SetFillStyle(0);
-  leg->SetBorderSize(0);
+void make2dplots(TString inputfile, TString plot, TString channel){
 
   TFile *f = new TFile(inputfile);
+
+  std::vector<TString> plots; plots << plot+" eff" << plot+" effMC" << "scalefactor "+plot;
+
+  TCanvas *c = new TCanvas();
+
+  for(unsigned int i=0;i<plots.size();i++){
+
+    TH2D * h = (TH2D*)f->Get(plots.at(i));
+   
+    gStyle->SetOptTitle(0);
+    gStyle->SetOptStat(0);
+   
+    h->GetYaxis()->SetTitleSize(0.06);
+    h->GetXaxis()->SetTitleSize(0.05);
+    h->GetYaxis()->SetLabelSize(0.05);
+    h->GetXaxis()->SetLabelSize(0.05);
+   
+    if(((TString)f->GetName()).Contains("mumu")){
+      h->GetXaxis()->SetTitle("#eta_{#mu_{1}}");
+      h->GetYaxis()->SetTitle("#eta_{#mu_{2}}");
+    }
+    else if(((TString)f->GetName()).Contains("emu")){
+      h->GetXaxis()->SetTitle("#eta_{e}");
+      h->GetYaxis()->SetTitle("#eta_{#mu}");
+    }
+    else if(((TString)f->GetName()).Contains("ee")){
+      h->GetXaxis()->SetTitle("#eta_{e_{1}}");
+      h->GetYaxis()->SetTitle("#eta_{e_{2}}");
+    }
+   
   
-  TH1D *h = (TH1D*)f->Get("axis "+add);
-
-  TGraphAsymmErrors *sfc=(TGraphAsymmErrors*) f->Get("scalefactor "+add+" incl corrErr");
-  TGraphAsymmErrors *sf=(TGraphAsymmErrors*) f->Get("scalefactor "+add);
-  TGraphAsymmErrors *d=(TGraphAsymmErrors*) f->Get(add+" eff");
-  TGraphAsymmErrors *mc=(TGraphAsymmErrors*) f->Get(add+" effMC");
-
-  h->SetAxisRange(0.6,1.1,"Y");
-  // set style
-   gStyle->SetOptTitle(0);
-   gStyle->SetOptStat(0);
-
-  sfc->SetLineColor(8);
-  sf ->SetLineColor(8);
-  mc->SetLineColor(kRed);
-  h->GetYaxis()->SetTitle("#epsilon,SF");
-  if(add=="pt") 
-    h->GetXaxis()->SetTitle("p_{T} [GeV]");
-  else if(add=="eta")
-    h->GetXaxis()->SetTitle("#eta");
-  else if(add=="dphi" || add=="dphi2"){
-    h->GetXaxis()->SetTitle("| #delta#phi_{l,met} |");
-  }
-  else if(add=="vmulti"){
-    h->GetXaxis()->SetTitle("n_{vtx}");
-  }
-  else if(add=="jetmulti"){
-    h->GetXaxis()->SetTitle("n_{jet}");
-  }
-
-  if(!add.Contains("2d")){
-  h->GetYaxis()->SetTitleSize(0.06);
-  h->GetXaxis()->SetTitleSize(0.05);
-  h->GetYaxis()->SetLabelSize(0.05);
-  h->GetXaxis()->SetLabelSize(0.05);
-
-  sfc->SetMarkerStyle(33);
-  sfc->SetMarkerColor(8);
-  sf->SetMarkerStyle(33);
-  sf->SetMarkerColor(8);
-  d->SetMarkerStyle(20);
-  d->SetMarkerColor(1);
-  mc->SetMarkerStyle(22);
-  mc->SetMarkerColor(kRed);
-
-  sfc->SetMarkerSize(1);
-  sf->SetMarkerSize(1);
-  d->SetMarkerSize(1);
-  mc->SetMarkerSize(1);
-
-  sfc->SetLineWidth(2);
-  sf->SetLineWidth(2);
-  d->SetLineWidth(2);
-  mc->SetLineWidth(2);
-
-  leg->AddEntry(sfc,"scale factor"  ,"pe");
-  leg->AddEntry(d,"data efficiency"  ,"pe");
-  leg->AddEntry(mc,"MC efficiency"  ,"pe");
-
-  gPad->SetLeftMargin(0.15);
-  gPad->SetBottomMargin(0.15);
-  h->Draw("AXIS");
-  sfc->Draw("P,e1,same");
-  sf->Draw("P,e1,same");
-  mc->Draw("P,e1,same");
-  d->Draw("P,e1,same");
-  leg->Draw("same");
-
-  }
-  ///2D stuff if f->GetName(Contains emu...
-
-  else if(add.Contains("2d")){
-
-
-    TH2D *hsf = (TH2D*)f->Get(add);
-    hsf->GetYaxis()->SetTitleSize(0.06);
-    hsf->GetXaxis()->SetTitleSize(0.05);
-    hsf->GetYaxis()->SetLabelSize(0.05);
-    hsf->GetXaxis()->SetLabelSize(0.05);
-    TString xaxistitle=(TString) hsf->GetXaxis()->GetTitle() + "_{e_1}";
-    TString yaxistitle=(TString) hsf->GetYaxis()->GetTitle() + "_{e_2}";
-    TString filename=f->GetName();
-    if(filename.Contains("_emu")){
-      xaxistitle=(TString) hsf->GetXaxis()->GetTitle() + "_{e}";
-      yaxistitle=(TString) hsf->GetYaxis()->GetTitle() + "_{#mu}";
-    }
-    else if(filename.Contains("_mumu")){
-      xaxistitle=(TString) hsf->GetXaxis()->GetTitle() + "_{#mu_1}";
-      yaxistitle=(TString) hsf->GetYaxis()->GetTitle() + "_{#mu_2}";
-    }
-
-    hsf->GetXaxis()->SetTitle(xaxistitle);
-    hsf->GetYaxis()->SetTitle(yaxistitle);
-
+   
+    //  h->Draw("colz,text,e");
     gPad->SetLeftMargin(0.15);
     gPad->SetBottomMargin(0.15);
-  
-    hsf->Draw("colz");
-    hsf->Draw("text,e,same");
-
+   
+   
+    h->Draw("colz,text,e");
+    c->Print(channel+ "_"+ plots.at(i)+".eps");
+    c->Print(channel+ "_"+ plots.at(i)+".pdf");
   }
-
-  //different markers
-  // dot data
-  //stuff for other
+  delete c;
 }
 
+void makeplot(TString inputfile, TString plot, TString channel){
 
-void miniscript(TString outdir){
+    TLegend * leg =  new TLegend(0.35,0.15,0.70,0.42);
+    leg->Clear();
+    leg->SetFillStyle(0);
+    leg->SetBorderSize(0);
 
-  // TString dir="/afs/naf.desy.de/user/k/kieseler/public/forCarmen/latestgreatesttriggerplots3/";
-  TString rootsdir=outdir;
 
-  std::vector<TString> channels, plots;
-  channels << "ee" << "mumu" << "emu" ;
-  plots << "pt" << "eta" << "dphi" << "dphi2" << "vmulti" << "jetmulti";
+    gStyle->SetOptTitle(0);
+    gStyle->SetOptStat(0);
 
-  TCanvas * c = new TCanvas("def","def");
-  c->Clear();
-  c->cd();
+    TFile *f = new TFile(inputfile);
 
-  for(unsigned int i=0;i<channels.size();i++){
-    for(unsigned int j=0;j<plots.size();j++){
-      makeplot(rootsdir+"triggerSummary_"+channels.at(i)+".root", plots.at(j));
-      c->Print(outdir+"corr_"+channels.at(i)+plots.at(j)+".pdf");
-      c->Print(outdir+"corr_"+channels.at(i)+plots.at(j)+".eps");
+    
+    if(!((TString)f->Get(plot +" eff")->ClassName()).Contains("TH2D")){
+  
+      TCanvas * c = new TCanvas();
       c->Clear();
+      c->cd();
+
+      TH1D *h = (TH1D*)f->Get("axis "+plot);
+
+      TGraphAsymmErrors *sfc=(TGraphAsymmErrors*) f->Get("scalefactor "+plot+" incl corrErr");
+      TGraphAsymmErrors *sf=(TGraphAsymmErrors*) f->Get("scalefactor "+plot);
+      TGraphAsymmErrors *d=(TGraphAsymmErrors*) f->Get(plot+" eff");
+      TGraphAsymmErrors *mc=(TGraphAsymmErrors*) f->Get(plot+" effMC");
+
+      h->SetAxisRange(0.5,1.1,"Y");
+      // set style
+      gStyle->SetOptTitle(0);
+      gStyle->SetOptStat(0);
+
+      sfc->SetLineColor(8);
+      sf ->SetLineColor(8);
+      mc->SetLineColor(kRed);
+      h->GetYaxis()->SetTitle("#epsilon,SF");
+      if(plot=="pt") 
+	h->GetXaxis()->SetTitle("p_{T} [GeV]");
+      else if(plot=="eta")
+	h->GetXaxis()->SetTitle("#eta");
+      else if(plot=="dphi" || plot=="dphi2"){
+	h->GetXaxis()->SetTitle("| #delta#phi_{l,met} |");
+      }
+      else if(plot=="vmulti"){
+	h->GetXaxis()->SetTitle("n_{vtx}");
+	//  h->SetAxisRange(0.4,1.1,"Y");
+	// h->SetAxisRange(0,30,"X");
+      }
+      else if(plot=="jetmulti"){
+	h->GetXaxis()->SetTitle("n_{jet}");
+      }
+      else if (plot="drlep"){
+	h->GetXaxis()->SetTitle("#DeltaR(l_{1},l_{2})");
+      }
+
+      h->GetYaxis()->SetTitleSize(0.06);
+      h->GetXaxis()->SetTitleSize(0.05);
+      h->GetYaxis()->SetLabelSize(0.05);
+      h->GetXaxis()->SetLabelSize(0.05);
+
+      sfc->SetMarkerStyle(33);
+      sfc->SetMarkerColor(8);
+      sf->SetMarkerStyle(33);
+      sf->SetMarkerColor(8);
+      d->SetMarkerStyle(20);
+      d->SetMarkerColor(1);
+      mc->SetMarkerStyle(22);
+      mc->SetMarkerColor(kRed);
+
+      sfc->SetMarkerSize(1);
+      sf->SetMarkerSize(1);
+      d->SetMarkerSize(1);
+      mc->SetMarkerSize(1);
+
+      sfc->SetLineWidth(2);
+      sf->SetLineWidth(2);
+      d->SetLineWidth(2);
+      mc->SetLineWidth(2);
+
+      leg->AddEntry(sfc,"scale factor"  ,"pe");
+      leg->AddEntry(d,"data efficiency"  ,"pe");
+      leg->AddEntry(mc,"MC efficiency"  ,"pe");
+
+      gPad->SetLeftMargin(0.15);
+      gPad->SetBottomMargin(0.15);
+      h->Draw("AXIS");
+      sfc->Draw("P,e1,same");
+      sf->Draw("P,e1,same");
+      mc->Draw("P,e1,same");
+      d->Draw("P,e1,same");
+      leg->Draw("same");
+
+      c->Print(channel+"_"+plot+".pdf");
+      c->Print(channel+"_"+plot+".eps");
+      delete c;
+
     }
+
+    else { //TH2D
+      make2dplots(inputfile, plot, channel);
+
+    }
+
+    //different markers
+    // dot data
+    //stuff for other
   }
+
+
+  void miniscript(){
+
+    TString outdir="";
+    // TString dir="/afs/naf.desy.de/user/k/kieseler/public/forCarmen/latestgreatesttriggerplots3/";
+    TString rootsdir=outdir;
+
+
+    std::vector<TString> channels, plots;
+    channels << "ee" << "mumu" << "emu" ;
+    plots << "eta2d" << "pt" << "eta" << "dphi" << "dphi2" << "vmulti" << "jetmulti" << "drlep";
+
+ 
+
+    for(unsigned int i=0;i<channels.size();i++){
+      for(unsigned int j=0;j<plots.size();j++){
+	makeplot(rootsdir+"triggerSummary_"+channels.at(i)+".root", plots.at(j),channels.at(i));
+      }
+    }
   
 
 
-}
+  }
 
 #endif
