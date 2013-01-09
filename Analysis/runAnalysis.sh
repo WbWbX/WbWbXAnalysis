627bfe6ebd0791f98715d7f46e55e2730f6e064c
jkiesele <>
2013-01-09 16:22:07 +0000

    fixed bug

--- Analysis/runAnalysis.sh
+++ Analysis/runAnalysis.sh
@@ -10,7 +10,7 @@ mkdir $dirstring
 cd $dirstring
 cp ../*inputfiles.txt .
 sed 's/runInNotQuietMode=true/runInNotQuietMode=false/' <../Analyzer.C > Analyzer.C
-sed 's/testmode=true/testmode=false/g' Analyzer.C
+sed -n s/testmode=true/testmode=false/gp Analyzer.C
 
 nohup root -l -b -q Analyzer.C++ > output.txt
 echo "done with ${dirstring}"
