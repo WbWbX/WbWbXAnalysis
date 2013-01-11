c63ee48dd2ea15472ec2691a5c73f4a79ce3db12
jkiesele <>
2013-01-11 10:49:39 +0000

    small test macro to check the inflated class

---
+++ DataFormats/test/infl.C
@@ -0,0 +1,38 @@
+#include "TTree.h"
+#include "TFile.h"
+#include <fstream>
+#include "TtZAnalysis/DataFormats/src/classes.h" 
+#include "TtZAnalysis/Tools/interface/containerStackVector.h"
+#include "TtZAnalysis/Tools/interface/miscUtils.h"
+
+
+void infl(){
+
+  using namespace top;
+  using namespace std;
+
+  TFile *f=TFile::Open("/scratch/hh/dust/naf/cms/user/kieseler/trees_8TeV_SUSY/tree_8TeV_singlee_runA_06Aug.root");
+
+  TTree * t = (TTree*) f->Get("PFTree/pfTree");
+  float n=t->GetEntries();
+
+  vector<NTInflatedElectron> * pElecs = 0;
+  t->SetBranchAddress("NTInflatedElectrons",&pElecs); 
+
+  vector<float> bins;
+  for(int i=0;i<32;i++) bins << i;
+
+
+  container1D cont(bins);
+
+  for(float i=0;i<n;i++){
+    t->GetEntry(i);
+    displayStatusBar(i,n);
+    for(unsigned int j=0;j<pElecs->size();j++)
+      cont.fill(pElecs->at(j).getMember("vbtf11WP80"));
+  }
+  cout << endl;
+  cont.getTH1D()->Draw();
+
+
+}
