ed229af807956f5f7bca7e050cbbfcd57352845d
jkiesele <>
2013-01-30 15:09:53 +0000

    initial commit of JER adjuster wrapper for NTJets

---
+++ DataFormats/interface/NTJERAdjuster.h
@@ -0,0 +1,28 @@
+#ifndef JERADJUSTER_H
+#define JERADJUSTER_H
+
+#include "TtZAnalysis/DataFormats/interface/NTJet.h"
+#include "TtZAnalysis/Tools/interface/JERBase.h"
+
+namespace top{
+
+  class NTJERAdjuster : public JERBase{
+
+  public:
+    NTJERAdjuster(){setSystematics("def");}
+    ~NTJERAdjuster(){}
+
+    void correctJets(std::vector<top::NTJet*> & jets){
+      for(size_t i=0;i<jets.size();i++){
+	correctJet(jets.at(i));
+      }
+    }
+    void correctJet(top::NTJet* jet){
+      top::LorentzVector temp=jet->p4();
+      correctP4(temp, jet->genP4());
+      jet->setP4(temp);
+    }
+
+  };
+}
+#endif
