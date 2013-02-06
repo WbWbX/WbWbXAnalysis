9603e3c3227add0a59b9553fd81a1901c823058a
jkiesele <>
2013-02-06 13:31:04 +0000

    top-12-007 follow-up

---
+++ Analysis/triggerEfficiencies12/runtrigger_12_007.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#runs analysis und puts analysis code and output to an extra directory with date flag
+
+dirstring=$1"_12028_trigger_$(date +%F_%H:%M)"
+
+echo '\n\nrunning in directory' "${dirstring}."
+
+mkdir $dirstring
+cd $dirstring
+cp ../do_triggerAnalyzer_12_028.C .
+cp ../makeplotsnice.h .
+cp ../make2dplots.h .
+nohup root -l -b -q do_triggerAnalyzer_12_028.C++ > output.txt 
+echo "done with ${dirstring}"
