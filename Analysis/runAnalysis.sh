2f158a7757ed9186fa9deaf66cbaf1ab1fb5ba85
jkiesele <>
2013-02-07 16:47:08 +0000

    now changes testmode

--- Analysis/runAnalysis.sh
+++ Analysis/runAnalysis.sh
@@ -9,8 +9,9 @@ echo "\n\nrunning in directory ${dirstring}."
 mkdir $dirstring
 cd $dirstring
 cp ../*inputfiles.txt .
-sed 's/runInNotQuietMode=true/runInNotQuietMode=false/' <../Analyzer.C > Analyzer.C
-sed -n s/testmode=true/testmode=false/gp Analyzer.C
+sed 's/runInNotQuietMode=true/runInNotQuietMode=false/' <../Analyzer.C > Analyzer_temp.C
+sed  's/testmode=true/testmode=false/' < Analyzer_temp.C > Analyzer.C
+rm Analyzer_temp.C
 
 nohup root -l -b -q Analyzer.C++ > output.txt
 echo "done with ${dirstring}"
