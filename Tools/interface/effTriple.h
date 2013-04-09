6fb752bc01e4ac2da10ed3e6bc1c6894027d4081
jkiesele <>
2013-04-09 19:37:21 +0000

    new stuff

---
+++ Tools/interface/effTriple.h
@@ -0,0 +1,224 @@
+#ifndef cefftriple_h
+#define cefftriple_h
+
+
+/// put possibility for TH2Ds and containers here
+// avoid dep to miscUtils.. aaaand on the .contrary..
+
+//#include "container.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TString.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+namespace ztop{
+
+
+  class histWrapper{
+  public:
+    histWrapper(){}
+    histWrapper(const TH1D & h, TString name=""){setHisto(h);setName(name);dividebinomial_=true;}
+    histWrapper(const TH2D & h, TString name=""){setHisto(h);setName(name);dividebinomial_=true;}
+    ~histWrapper(){}
+
+    void setHisto(const TH2D & h2d){th2d_=h2d;is2d_=true;}
+    void setHisto(const TH1D & h){th1d_=h;is2d_=false;}
+    void setName(TString name){
+      name_=name;
+      if(isTH1D()){
+	th1d_.SetName(name_);
+	th1d_.SetTitle(name_);
+      }
+      else{
+	th2d_.SetName(name_);
+	th2d_.SetTitle(name_);
+      }
+    }
+    void setDivideBinomial(bool div){dividebinomial_=div;}
+
+    bool isTH1D(){return !is2d_;}
+    bool isTH2D(){return is2d_;}
+
+    TH2D & getTH2D()  {return th2d_;}
+    TH1D & getTH1D()  {return th1d_;}
+    TString getName(){return name_;}
+
+    void fill(double val, double val2weight=1, double weight=1){
+      if(isTH1D())
+	th1d_.Fill(val, val2weight);
+      else
+	th2d_.Fill(val, val2weight, weight);
+    }
+
+    void write(){
+     if(isTH1D())
+       th1d_.Write();
+     else
+       th2d_.Write();
+    }
+    void draw(TString opt=""){
+      if(isTH1D())
+       th1d_.Draw(opt);
+     else
+       th2d_.Draw(opt);
+    }
+
+    histWrapper operator / (histWrapper & denominator){ // only binomial division
+      histWrapper def;
+      if(isTH1D() != denominator.isTH1D()){
+	std::cout << "histWrapper operator /: tried to divide different histo types, returning empty histogram!" << std::endl;
+	return def;
+      }
+      if(isTH1D() && denominator.isTH1D()){
+	if(th1d_.GetNbinsX() != denominator.th1d_.GetNbinsX()){
+	  std::cout << "histWrapper operator /: tried to divide different histo binnings, returning empty histogram!" << std::endl;
+	  return def;
+	}
+	//do binomial
+	TH1D h=divideTH1D(getTH1D(),denominator.getTH1D(), dividebinomial_);
+	histWrapper out(h);
+	return out;
+      }
+      else if(isTH2D() && denominator.isTH2D()){
+	if(th2d_.GetNbinsX() != denominator.th2d_.GetNbinsX() || th2d_.GetNbinsY() != denominator.th2d_.GetNbinsY()){
+	  std::cout << "histWrapper operator /: tried to divide different histo binnings, returning empty histogram!" << std::endl;
+	  return def;
+	}
+	//do binomial
+	TH2D h=divideTH2D(getTH2D(),denominator.getTH2D(), dividebinomial_);
+	histWrapper out(h);
+	return out;
+      }
+      return def;
+    }
+
+  private:
+    TString name_;
+
+    TH2D th2d_;
+    TH1D th1d_;
+
+    bool is2d_;
+
+    bool dividebinomial_;
+
+    TH1D divideTH1D(const TH1D &h1, const TH1D &h2, bool binomial=true){ //! out = h1 / h2
+      TH1D out=h1; 
+      for(int binx=1;binx<=h1.GetNbinsX()+1;binx++){
+	double cont=0;
+	double err=0;
+	if(h2.GetBinContent(binx) !=0){
+	  cont=h1.GetBinContent(binx) / h2.GetBinContent(binx);
+	  double err1=h1.GetBinError(binx)/h2.GetBinContent(binx);
+	  double err2=cont/h2.GetBinContent(binx) * h2.GetBinError(binx);
+	  if(binomial)
+	    err=sqrt(cont*(1-cont)/ h1.GetBinContent(binx));
+	  else
+	    err=sqrt(err1*err1 + err2*err2);
+	}
+	out.SetBinContent(binx,cont);
+	out.SetBinError(binx,err);
+      }  
+      return out;
+    }
+
+
+    TH2D divideTH2D(const TH2D &h1, const TH2D &h2, bool binomial=true){ //! out = h1 / h2
+      TH2D out=h1; 
+      for(int binx=1;binx<=h1.GetNbinsX()+1;binx++){
+	for(int biny=1;biny<=h1.GetNbinsY()+1;biny++){
+	  double cont=0;
+	  double err=0;
+	  if(h2.GetBinContent(binx,biny) !=0){
+	    cont=h1.GetBinContent(binx,biny) / h2.GetBinContent(binx,biny);
+	    double err1=h1.GetBinError(binx,biny)/h2.GetBinContent(binx,biny);
+	    double err2=cont/h2.GetBinContent(binx,biny) * h2.GetBinError(binx,biny);
+	    if(binomial)
+	      err=sqrt(cont*(1-cont)/ h1.GetBinContent(binx,biny));
+	    else
+	      err=sqrt(err1*err1 + err2*err2);
+	  }
+	  out.SetBinContent(binx,biny,cont);
+	  out.SetBinError(binx,biny,err);
+	}
+      }
+      return out;
+    }
+
+  };
+
+
+  ///////
+
+  class effTriple{
+  public:
+    effTriple();
+    effTriple(std::vector<float> binsx,  
+	      TString name="",
+	      TString xaxisname="",
+	      TString yaxisname="", 
+	      bool mergeufof=false, 
+	      size_t size=3); //! bins, name, xaxisname, yaxisname, mergeUFOF=false - for 1D histos
+    effTriple(std::vector<float> binsx,  
+	      std::vector<float> binsy, 
+	      TString name="",
+	      TString xaxisname="",
+	      TString yaxisname="", 
+	      bool mergeufof=false, 
+	      size_t size=3); //! binsx, binsy, name, xaxisname, yaxisname, mergeUFOF=false
+    
+    ~effTriple(); //! delete pointers in efflist
+
+    void fillNum(double value, double val2weight=1, double weight=1);
+    void fillDen(double value, double val2weight=1, double weight=1);
+
+    
+    histWrapper getEff(){makeEff(); return hists_.at(2);}
+    histWrapper getNum(){return hists_.at(0);}
+    histWrapper getDen(){return hists_.at(1);}
+
+    TString getName(){return name_;}
+
+    static std::vector<effTriple *> effTriple_list;
+    static bool makelist;
+
+  protected:
+    //see where to put these things. first everything here
+    // map with histos etc. possibility to add more than 3 (for corr or something) and to define operations(?) to be implemented later
+
+    TString name_;
+    std::vector<histWrapper> hists_;
+
+
+  private:
+    //les see what comes here
+    void addToList();
+    void check1d();
+    void check2d();
+    void makeEff();
+    void setStyle2d(TH2D &){}; //can do
+    void setStyle(TH1D &){};   //can do
+
+  };
+
+  //Some helper functions:
+  /*
+  vector? histWrapper makeSFs(vector<effTriple> num, vector<effTriple> den){
+
+    // for each check whether 2d or 1D etc
+    //make SF and 
+
+  }
+
+
+  
+
+//afterwards make functions for triggeranalyzer/triggeranalyzer to produce the set of SF plots
+
+  */
+
+}
+#endif
+
---
+++ Tools/src/effTriple.cc
@@ -0,0 +1,144 @@
+#include "../interface/effTriple.h"
+
+////////////////////CUT//////////////////
+
+
+namespace ztop{
+
+  std::vector<effTriple *> effTriple::effTriple_list;
+  bool effTriple::makelist=false;
+
+
+  void effTriple::addToList(){
+    if(makelist)
+      effTriple_list.push_back(this);
+  }
+
+  void effTriple::check1d(){
+    if(!hists_.at(0).isTH1D()){
+      std::cout << "effTriple::XX: 1D Histograms not initialized! exit!" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  }
+
+  void effTriple::check2d(){
+    if(!hists_.at(0).isTH2D()){
+      std::cout << "effTriple::XX: 2D Histograms not initialized! exit!" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  }
+
+  void effTriple::makeEff(){
+    if(hists_.size() < 2)
+      std::exit(EXIT_FAILURE); //should not happen because default constructor is disabled
+
+    histWrapper eff=hists_.at(0) / hists_.at(1);
+    eff.setName(name_+"_eff");
+    eff.setDivideBinomial(false);
+    if(hists_.size() < 3)
+      hists_.push_back(eff);
+    else
+      hists_.at(2) = eff;
+    hists_.at(2).setName(name_+"_eff");
+    std::cout << "eff made for " << hists_.at(2).getName() << std::endl;
+  }
+
+
+  ///public functions
+
+  effTriple::effTriple(){
+    std::cout << "effTriple::effTriple(): default constructor not supported (yet), exit" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  effTriple::effTriple(std::vector<float> binsx,  TString name,TString xaxisname,TString yaxisname, bool mergeufof, size_t size){
+    if(size>3){
+      std::cout << "effTriple:size>3 not supported yet!" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    name_=name;
+    TH1::AddDirectory(kFALSE); 
+    double binxarray[binsx.size()];
+    for(size_t i=0;i<binsx.size();i++){
+      binxarray[i] = binsx[i];
+    }
+    for(size_t i=0;i<size;i++){
+      TString add;
+      if(i==0) add="_num";
+      else if(i==(size_t)1) add="_den";
+      else if(i==(size_t)2) add="_eff";
+      TH1D h=TH1D(name+add,
+		  name+add,
+		  binsx.size()-1,
+		  binxarray);
+      h.GetXaxis()->SetTitle(xaxisname);
+      h.GetYaxis()->SetTitle(yaxisname);
+      setStyle(h);
+      histWrapper temp(h,name+add);
+      hists_.push_back(temp);
+    }
+    bool justAPlaceholderIgnoreWarning=mergeufof; //to avoid warning !IMPLEMENT! later (or remove switch)
+    addToList();
+  }
+
+  effTriple::effTriple(std::vector<float> binsx,  std::vector<float> binsy, TString name,TString xaxisname,TString yaxisname, bool mergeufof, size_t size){
+    if(size>3){
+      std::cout << "effTriple:size>3 not supported yet!" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    name_=name;
+    TH1::AddDirectory(kFALSE); 
+    double binxarray[binsx.size()];
+    for(size_t i=0;i<binsx.size();i++){
+      binxarray[i] = binsx[i];
+    }
+   
+    double binyarray[binsy.size()];
+    for(size_t i=0;i<binsy.size();i++){
+      binyarray[i] = binsy[i];
+    }
+    
+    for(size_t i=0;i<size;i++){
+      TString add;
+      if(i==0) add="_num";
+      else if(i==(size_t)1) add="_den";
+      else if(i==(size_t)2) add="_eff";
+      TH2D h=TH2D(name+add,
+		  name+add,
+		  binsx.size()-1,
+		  binxarray,
+		  binsy.size()-1,
+		  binyarray);
+      h.GetXaxis()->SetTitle(xaxisname);
+      h.GetYaxis()->SetTitle(yaxisname);
+      setStyle2d(h);
+      histWrapper temp(h,name+add);
+      hists_.push_back(temp);
+    }
+    bool justAPlaceholderIgnoreWarning=mergeufof; //to avoid warning !IMPLEMENT! later (or remove switch)
+    addToList();
+  }
+
+  effTriple::~effTriple(){
+    for(size_t i=0;i<effTriple_list.size();i++){
+      if(effTriple_list[i] == this){
+	effTriple_list.erase(effTriple_list.begin()+i);
+	break;
+      }
+    }
+  }
+
+  void effTriple::fillNum(double value, double val2weight, double weight){
+    hists_.at(0).fill(value, val2weight, weight);
+  }
+
+  void effTriple::fillDen(double value,double  val2weight, double weight){
+    hists_.at(1).fill(value, val2weight, weight);
+  }
+
+
+
+
+} //namespace
+
+
