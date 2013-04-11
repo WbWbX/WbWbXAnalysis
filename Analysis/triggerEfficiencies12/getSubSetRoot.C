ca5888b258964e5f265ad7f447c06d5e7926ff89
jkiesele <>
2013-04-11 10:30:17 +0000

    added eff plots

--- Analysis/triggerEfficiencies12/getSubSetRoot.C
+++ Analysis/triggerEfficiencies12/getSubSetRoot.C
@@ -2,6 +2,7 @@
 #include "TH1D.h"
 #include "TH2D.h"
 #include "TopAnalysis/ZTopUtils/interface/miscUtils.h"
+#include <iostream>
 
 void getSubSetRoot(TString dirname){
   using namespace ztop;
@@ -13,8 +14,11 @@ void getSubSetRoot(TString dirname){
 
 
  
+
   TFile *f =new TFile(dirname+"/raw.root");
   TFile *fout = new TFile("plotHistos.root","RECREATE");
+
+  //get num and den
 
   std::vector<TString> adds;
   adds << "_num" << "_den" << "_num_mc" << "_den_mc";
@@ -29,4 +33,23 @@ void getSubSetRoot(TString dirname){
       obj->Write();
     }
   }
+
+  TFile *f3 =new TFile(dirname+"/scalefactors.root");
+  std::vector<TString> addssf;
+  addssf << "_eff" << "_eff_mc";
+
+  
+  for(size_t i=0;i<plots.size();i++){
+    TString plotname=plots.at(i);
+    for(size_t j=0;j<addssf.size();j++){
+      TString name=plotname+addssf.at(j);
+      TObject * obj = f3->Get(name);
+      fout->cd();
+      obj->Write();
+    }
+  }
+  
 }
+
+
+// run number runcrecoverbla rereco 201191 one single run
