d9d2950f43cbda3993fe9e669b1f7080d82e848d
jkiesele <>
2013-04-11 16:31:07 +0000

    nicer path stuff2

--- Data/pileupcalc.sh
+++ Data/pileupcalc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pileupJson=$1
-analysisJson=$2
+pileupJson=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/PileUp/pileup_latest.txt
+analysisJson=$1
 
 #default
 
@@ -11,11 +11,11 @@ pileupCalc.py --minBiasXsec 69400 --inputLumiJSON $pileupJson --maxPileupBin 50 
 
 #up
 
-#pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_up.root
+pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_up.root
 
 #down
 
-#pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_down.root
+pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_down.root
 
 #up: 72870
 #down 65930
