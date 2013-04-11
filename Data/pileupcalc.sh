9f0a2027331c47e266b541c1180fc1d30bb76390
jkiesele <>
2013-04-11 16:30:00 +0000

    nicer path stuff

--- Data/pileupcalc.sh
+++ Data/pileupcalc.sh
@@ -5,15 +5,17 @@ analysisJson=$2
 
 #default
 
-pileupCalc.py --minBiasXsec 69400 --inputLumiJSON $pileupJson --maxPileupBin 50 --numPileupBins 50 --calcMode true -i $analysisJson ${analysisJson}_PU.root
+output=`echo ${analysisJson##*/}`
+
+pileupCalc.py --minBiasXsec 69400 --inputLumiJSON $pileupJson --maxPileupBin 50 --numPileupBins 50 --calcMode true -i $analysisJson ${output}_PU.root
 
 #up
 
-pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${analysisJson}_PU_up.root
+#pileupCalc.py --minBiasXsec 72870 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_up.root
 
 #down
 
-pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${analysisJson}_PU_down.root
+#pileupCalc.py --minBiasXsec 65930 --inputLumiJSON $pileupJson --maxPileupBin 70 --numPileupBins 70 --calcMode true -i $analysisJson ${output}_PU_down.root
 
 #up: 72870
 #down 65930
