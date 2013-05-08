face717fab2f6e27b1b0bf480df08a74bbcb0d36
jkiesele <>
2013-05-08 17:13:49 +0000

    includes

--- DataFormats/interface/mathdefs.h
+++ DataFormats/interface/mathdefs.h
@@ -2,7 +2,11 @@
 #define MATHDEFS_H
 
 //#include "Math/GenVector/LorentzVector.h"
-#include "DataFormats/Math/interface/LorentzVector.h"
+//#include "DataFormats/Math/interface/LorentzVector.h"
+
+
+#include <Math/LorentzVector.h>
+#include <Math/PtEtaPhiM4D.h>
 
 namespace ztop{
   typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzVector;
