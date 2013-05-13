928e4254091f154ae40aa8389054abdef1d2d3f3
jkiesele <>
2013-05-13 07:44:54 +0000

    new stuff

--- Analysis/triggerEfficiencies12/trigger_looseMu.C
+++ Analysis/triggerEfficiencies12/trigger_looseMu.C
@@ -67,6 +67,15 @@ void trigger_looseMu(){
   binsmumueta << -2.4 << -2.1 << -1.2 << -0.9 << 0.9 << 1.2 << 2.1 << 2.4;
   bins2dmumu << 0 << 0.9 << 1.2 << 2.1 << 2.4;
 
+
+
+  double lumiA=0.890;
+  double lumiB=4.430;
+  double lumiC=7.026;
+  double lumiD=7.272;
+
+  double lumitotal=lumiA+lumiB+lumiC+lumiD;
+
   triggerAnalyzer ta_mumud;
 
   ta_mumud.setMode("mumu");
@@ -86,7 +95,7 @@ void trigger_looseMu(){
 
 
   
-  TString dir="/scratch/hh/dust/naf/cms/user/kieseler/trees_PS_trig_03/";
+  TString dir="/scratch/hh/dust/naf/cms/user/kieseler/trees_Apr13_04/";
 
   TString cmssw_base=getenv("CMSSW_BASE");
   TString pileuproot = cmssw_base+"/src/TtZAnalysis/Data/Full19.json.txt_PU.root";
@@ -103,159 +112,213 @@ void trigger_looseMu(){
 		 << dir + "tree_8TeV_MET_runC_prompt.root"
 		 << dir + "tree_8TeV_MET_runC_24Aug.root"  
 		 << dir + "tree_8TeV_MET_runC_11Dec.root"  
-		 << dir + "tree_8TeV_MET_runD_prompt.root";
+  // 		 << dir + "tree_8TeV_MET_runD_prompt.root";
   
 
-  datafilesRuna << dir + "tree_8TeV_MET_runA_06Aug.root"
-		<< dir + "tree_8TeV_MET_runA_13Jul.root" ;
+  // datafilesRuna << dir + "tree_8TeV_MET_runA_06Aug.root"
+  // 		<< dir + "tree_8TeV_MET_runA_13Jul.root" ;
 
-  datafilesRunb << dir + "tree_8TeV_MET_runB_13Jul.root"  ;
-  datafilesRunc << dir + "tree_8TeV_MET_runC_24Aug.root"  
-		<< dir + "tree_8TeV_MET_runC_11Dec.root" 
-		<< dir + "tree_8TeV_MET_runC_prompt.root" ; 
+  // datafilesRunb << dir + "tree_8TeV_MET_runB_13Jul.root"  ;
+  // datafilesRunc << dir + "tree_8TeV_MET_runC_24Aug.root"  
+  // 		<< dir + "tree_8TeV_MET_runC_11Dec.root" 
+  // 		<< dir + "tree_8TeV_MET_runC_prompt.root" ; 
 
-  datafilesRund << dir + "tree_8TeV_MET_runD_prompt.root" ; 
+  // datafilesRund << dir + "tree_8TeV_MET_runD_prompt.root" ; 
 
 
 
-  //for others just copy data and mc, change input and pu file
+  // //for others just copy data and mc, change input and pu file
  
 
-  TChain * datachainFull=makeChain(datafilesFull);
-  TChain * datachainRunA=makeChain(datafilesRuna);
-  TChain * datachainRunB=makeChain(datafilesRunb);
-  TChain * datachainRunC=makeChain(datafilesRunc);
-  TChain * datachainRunD=makeChain(datafilesRund);
-  TChain * mumuchain=makeChain(mumumcfiles);
+  // TChain * datachainFull=makeChain(datafilesFull);
+  // TChain * datachainRunA=makeChain(datafilesRuna);
+  // TChain * datachainRunB=makeChain(datafilesRunb);
+  // TChain * datachainRunC=makeChain(datafilesRunc);
+  // TChain * datachainRunD=makeChain(datafilesRund);
+  // TChain * mumuchain=makeChain(mumumcfiles);
 
-  ta_mumud.setChain(datachainFull);
-  ta_mumuMC.setChain(mumuchain);
-  ta_mumuMC.setPUFile(pileuproot);
+  // ta_mumud.setChain(datachainFull);
+  // ta_mumuMC.setChain(mumuchain);
+  // ta_mumuMC.setPUFile(pileuproot);
  
   
-  ta_mumud.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumud, ta_mumuMC, "fullMu17Mu8", "19 fb^{-1}, Mu17Mu8", 0.01);
+  // //  ta_mumud.Eff();
+  // //  ta_mumuMC.Eff();
+  // //  makeFullOutput(ta_mumud, ta_mumuMC, "fullMu17Mu8", "19 fb^{-1}, Mu17Mu8", 0.01);
   
   
-  //Run A
+  // //Run A
 
-  triggerAnalyzer ta_mumuRunA=ta_mumud;
-  ta_mumuRunA.setChain(datachainRunA);
-  ta_mumuRunA.setRunCutLow(190456);
-  ta_mumuRunA.setRunCutHigh(193621);
+  // triggerAnalyzer ta_mumudA=ta_mumud;
+  // ta_mumudA.setChain(datachainRunA);
+  // ta_mumudA.setRunCutLow(190456);
+  // ta_mumudA.setRunCutHigh(193621);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
-  ta_mumuRunA.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunA, ta_mumuMC, "RunAMu17Mu8", "Run A, Mu17Mu8", 0.01);
+  // triggerAnalyzer ta_mumuMCA=ta_mumuMC;
+  // ta_mumuMCA.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
+  // ta_mumudA.Eff();
+  // ta_mumuMCA.Eff();
+  // makeFullOutput(ta_mumudA, ta_mumuMCA, "RunAMu17Mu8", "Run A, Mu17Mu8", 0.01);
 
   
 
-  //Run B
+  // //Run B
 
-  triggerAnalyzer ta_mumuRunB=ta_mumud;
-  ta_mumuRunB.setChain(datachainRunB);
-  ta_mumuRunB.setRunCutLow(193834);
-  ta_mumuRunB.setRunCutHigh(196531);
+  // triggerAnalyzer ta_mumudB=ta_mumud;
+  // ta_mumudB.setChain(datachainRunB);
+  // ta_mumudB.setRunCutLow(193834);
+  // ta_mumudB.setRunCutHigh(196531);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
-  ta_mumuRunB.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunB, ta_mumuMC, "RunBMu17Mu8", "Run B, Mu17Mu8", 0.01);
+  // triggerAnalyzer ta_mumuMCB=ta_mumuMC;
+  // ta_mumuMCB.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
+  // ta_mumudB.Eff();
+  // ta_mumuMCB.Eff();
+  // makeFullOutput(ta_mumudB, ta_mumuMCB, "RunBMu17Mu8", "Run B, Mu17Mu8", 0.01);
   
 
-  //Run C
+  // //Run C
 
-  triggerAnalyzer ta_mumuRunC=ta_mumud;
-  ta_mumuRunC.setChain(datachainRunC);
-  ta_mumuRunC.setRunCutLow(198022);
-  ta_mumuRunC.setRunCutHigh(203746);
+  // triggerAnalyzer ta_mumudC=ta_mumud;
+  // ta_mumudC.setChain(datachainRunC);
+  // ta_mumudC.setRunCutLow(198022);
+  // ta_mumudC.setRunCutHigh(203746);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunCComp.json.txt_PU.root");
-  ta_mumuRunC.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunC, ta_mumuMC, "RunCMu17Mu8", "Run C, Mu17Mu8", 0.01);
+  // triggerAnalyzer ta_mumuMCC=ta_mumuMC;
+  // ta_mumuMCC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunCComp.json.txt_PU.root");
+  // ta_mumudC.Eff();
+  // ta_mumuMCC.Eff();
+  // makeFullOutput(ta_mumudC, ta_mumuMCC, "RunCMu17Mu8", "Run C, Mu17Mu8", 0.01);
 
 
-  //Run D
+  // //Run D
   
-  triggerAnalyzer ta_mumuRunD=ta_mumud;
-  ta_mumuRunD.setChain(datachainRunD);
-  ta_mumuRunD.setRunCutLow(203768);
-  ta_mumuRunD.setRunCutHigh(208686);
+  // triggerAnalyzer ta_mumudD=ta_mumud;
+  // ta_mumudD.setChain(datachainRunD);
+  // ta_mumudD.setRunCutLow(203768);
+  // ta_mumudD.setRunCutHigh(208686);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
-  ta_mumuRunD.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunD, ta_mumuMC, "RunDMu17Mu8", "Run D, Mu17Mu8", 0.01);
+  // triggerAnalyzer ta_mumuMCD=ta_mumuMC;
+  // ta_mumuMCD.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
+  // ta_mumudD.Eff();
+  // ta_mumuMCD.Eff();
+  // makeFullOutput(ta_mumudD, ta_mumuMCD, "RunDMu17Mu8", "Run D, Mu17Mu8", 0.01);
 
-  ///////other trigger: Mu17_TkMu8
+
+  // //combine
+
+  // double mumutotal=ta_mumudA.getGlobalDen() + ta_mumudB.getGlobalDen() + ta_mumudC.getGlobalDen() + ta_mumudD.getGlobalDen();
+  // double mumuAscale=mumutotal/ta_mumudA.getGlobalDen() * lumiA/lumitotal;
+  // ta_mumudA.scale(mumuAscale);
+  // ta_mumuMCA.scale(mumuAscale);
+  // double mumuBscale=mumutotal/ta_mumudB.getGlobalDen() * lumiB/lumitotal;
+  // ta_mumudB.scale(mumuBscale);
+  // ta_mumuMCB.scale(mumuBscale);
+  // double mumuCscale=mumutotal/ta_mumudC.getGlobalDen() * lumiC/lumitotal;
+  // ta_mumudC.scale(mumuCscale);
+  // ta_mumuMCC.scale(mumuCscale);
+  // double mumuDscale=mumutotal/ta_mumudD.getGlobalDen() * lumiD/lumitotal;
+  // ta_mumudD.scale(mumuDscale);
+  // ta_mumuMCD.scale(mumuDscale);
+
+
+  // triggerAnalyzer mumudFull = ta_mumudA + ta_mumudB + ta_mumudC + ta_mumudD;
+  // triggerAnalyzer mumumcFull = ta_mumuMCA + ta_mumuMCB + ta_mumuMCC + ta_mumuMCD;
+  // makeFullOutput(mumudFull, mumumcFull, "FullMu17Mu8", "Mu17Mu8, 19 fb^{-1}", 0.01);
+
+
+  // ///////other trigger: Mu17_TkMu8
   
   
-  ta_mumud.setDileptonTrigger("HLT_Mu17_TkMu8_v");
-  ta_mumuMC.setDileptonTrigger("HLT_Mu17_TkMu8_v");
+  // ta_mumud.setDileptonTrigger("HLT_Mu17_TkMu8_v");
+  // ta_mumuMC.setDileptonTrigger("HLT_Mu17_TkMu8_v");
 
 
-  ta_mumud.setChain(datachainFull);
-  ta_mumuMC.setChain(mumuchain);
-  ta_mumuMC.setPUFile(pileuproot);
+  // ta_mumud.setChain(datachainFull);
+  // ta_mumuMC.setChain(mumuchain);
+  // ta_mumuMC.setPUFile(pileuproot);
  
   
-  ta_mumud.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumud, ta_mumuMC, "fullMu17TkMu8", "19 fb^{-1}, Mu17TkMu8", 0.01);
+  // // ta_mumud.Eff();
+  // //  ta_mumuMC.Eff();
+  // // makeFullOutput(ta_mumud, ta_mumuMC, "fullMu17TkMu8", "19 fb^{-1}, Mu17TkMu8", 0.01);
 
   
-  //Run A 
+  // //Run A 
   
-  ta_mumuRunA=ta_mumud;
-  ta_mumuRunA.setChain(datachainRunA);
-  ta_mumuRunA.setRunCutLow(190456);
-  ta_mumuRunA.setRunCutHigh(193621);
+  // ta_mumudA=ta_mumud;
+  // ta_mumudA.setChain(datachainRunA);
+  // ta_mumudA.setRunCutLow(190456);
+  // ta_mumudA.setRunCutHigh(193621);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
-  ta_mumuRunA.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunA, ta_mumuMC, "RunAMu17TkMu8", "Run A, Mu17TkMu8", 0.01);
+  // ta_mumuMCA=ta_mumuMC;
+  // ta_mumuMCA.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
+  // ta_mumudA.Eff();
+  // ta_mumuMCA.Eff();
+  // makeFullOutput(ta_mumudA, ta_mumuMCA, "RunAMu17TkMu8", "Run A, Mu17TkMu8", 0.01);
 
-  //Run B
+  // //Run B
 
-  ta_mumuRunB=ta_mumud;
-  ta_mumuRunB.setChain(datachainRunB);
-  ta_mumuRunB.setRunCutLow(193834);
-  ta_mumuRunB.setRunCutHigh(196531);
+  // ta_mumudB=ta_mumud;
+  // ta_mumudB.setChain(datachainRunB);
+  // ta_mumudB.setRunCutLow(193834);
+  // ta_mumudB.setRunCutHigh(196531);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
-  ta_mumuRunB.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunB, ta_mumuMC, "RunBMu17TkMu8", "Run B, Mu17TkMu8", 0.01);
+  // ta_mumuMCB=ta_mumuMC;
+  // ta_mumuMCB.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
+  // ta_mumudB.Eff();
+  // ta_mumuMCB.Eff();
+  // makeFullOutput(ta_mumudB, ta_mumuMCB, "RunBMu17TkMu8", "Run B, Mu17TkMu8", 0.01);
 
-  //Run C
+  // //Run C
 
-  ta_mumuRunC=ta_mumud;
-  ta_mumuRunC.setChain(datachainRunC);
-  ta_mumuRunC.setRunCutLow(198022);
-  ta_mumuRunC.setRunCutHigh(203746);
+  // ta_mumudC=ta_mumud;
+  // ta_mumudC.setChain(datachainRunC);
+  // ta_mumudC.setRunCutLow(198022);
+  // ta_mumudC.setRunCutHigh(203746);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunCComp.json.txt_PU.root");
-  ta_mumuRunC.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunC, ta_mumuMC, "RunCMu17TkMu8", "Run C, Mu17TkMu8", 0.01);
+  // ta_mumuMCC=ta_mumuMC;
+  // ta_mumuMCC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunCComp.json.txt_PU.root");
+  // ta_mumudC.Eff();
+  // ta_mumuMCC.Eff();
+  // makeFullOutput(ta_mumudC, ta_mumuMCC, "RunCMu17TkMu8", "Run C, Mu17TkMu8", 0.01);
 
-  //Run D
+  // //Run D
 
-  ta_mumuRunD=ta_mumud;
-  ta_mumuRunD.setChain(datachainRunD);
-  ta_mumuRunD.setRunCutLow(203768);
-  ta_mumuRunD.setRunCutHigh(208686);
+  // ta_mumudD=ta_mumud;
+  // ta_mumudD.setChain(datachainRunD);
+  // ta_mumudD.setRunCutLow(203768);
+  // ta_mumudD.setRunCutHigh(208686);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
-  ta_mumuRunD.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunC, ta_mumuMC, "RunDMu17TkMu8", "Run D, Mu17TkMu8", 0.01);
+  // ta_mumuMCD=ta_mumuMC;
+  // ta_mumuMCD.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
+  // ta_mumudD.Eff();
+  // ta_mumuMCD.Eff();
+  // makeFullOutput(ta_mumudC, ta_mumuMCD, "RunDMu17TkMu8", "Run D, Mu17TkMu8", 0.01);
 
   
+  // ///combine
+
+  // mumutotal=ta_mumudA.getGlobalDen() + ta_mumudB.getGlobalDen() + ta_mumudC.getGlobalDen() + ta_mumudD.getGlobalDen();
+  // mumuAscale=mumutotal/ta_mumudA.getGlobalDen() * lumiA/lumitotal;
+  // ta_mumudA.scale(mumuAscale);
+  // ta_mumuMCA.scale(mumuAscale);
+  // mumuBscale=mumutotal/ta_mumudB.getGlobalDen() * lumiB/lumitotal;
+  // ta_mumudB.scale(mumuBscale);
+  // ta_mumuMCB.scale(mumuBscale);
+  // mumuCscale=mumutotal/ta_mumudC.getGlobalDen() * lumiC/lumitotal;
+  // ta_mumudC.scale(mumuCscale);
+  // ta_mumuMCC.scale(mumuCscale);
+  // mumuDscale=mumutotal/ta_mumudD.getGlobalDen() * lumiD/lumitotal;
+  // ta_mumudD.scale(mumuDscale);
+  // ta_mumuMCD.scale(mumuDscale);
+
+
+  // mumudFull = ta_mumudA + ta_mumudB + ta_mumudC + ta_mumudD;
+  // mumumcFull = ta_mumuMCA + ta_mumuMCB + ta_mumuMCC + ta_mumuMCD;
+  // makeFullOutput(mumudFull, mumumcFull, "FullMu17TkMu8", "Mu17TkMu8, 19 fb^{-1}", 0.01);
+
+
+
 
   std::vector<string> bothtrig;
   bothtrig << "HLT_Mu17_TkMu8_v" << "HLT_Mu17_Mu8_v";
@@ -270,63 +333,91 @@ void trigger_looseMu(){
   ta_mumuMC.setPUFile(pileuproot);
  
   
-  ta_mumud.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumud, ta_mumuMC, "fullOR", "19 fb^{-1}, Mu17TkMu8 OR Mu17Mu8", 0.01);
+  //  ta_mumud.Eff();
+  //  ta_mumuMC.Eff();
+  //  makeFullOutput(ta_mumud, ta_mumuMC, "fullOR", "19 fb^{-1}, Mu17TkMu8 OR Mu17Mu8", 0.01);
 
   
   //Run A 
 
   
-  ta_mumuRunA=ta_mumud;
-  ta_mumuRunA.setChain(datachainRunA);
-  ta_mumuRunA.setRunCutLow(190456);
-  ta_mumuRunA.setRunCutHigh(193621);
+  ta_mumudA=ta_mumud;
+  ta_mumudA.setChain(datachainRunA);
+  ta_mumudA.setRunCutLow(190456);
+  ta_mumudA.setRunCutHigh(193621);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
-  ta_mumuRunA.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunA, ta_mumuMC, "RunAOR", "Run A,  Mu17TkMu8 OR Mu17Mu8", 0.01);
+  ta_mumuMCA=ta_mumuMC;
+  ta_mumuMCA.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunAComp.json.txt_PU.root");
+  ta_mumudA.Eff();
+  ta_mumuMCA.Eff();
+  makeFullOutput(ta_mumudA, ta_mumuMCA, "RunAOR", "Run A,  Mu17TkMu8 OR Mu17Mu8", 0.01);
 
   //Run B
 
-  ta_mumuRunB=ta_mumud;
-  ta_mumuRunB.setChain(datachainRunB);
-  ta_mumuRunB.setRunCutLow(193834);
-  ta_mumuRunB.setRunCutHigh(196531);
+  ta_mumudB=ta_mumud;
+  ta_mumudB.setChain(datachainRunB);
+  ta_mumudB.setRunCutLow(193834);
+  ta_mumudB.setRunCutHigh(196531);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
-  ta_mumuRunB.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunB, ta_mumuMC, "RunBOR", "Run B,  Mu17TkMu8 OR Mu17Mu8", 0.01);
+  ta_mumuMCB=ta_mumuMC;
+  ta_mumuMCB.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunB13Jul.json.txt_PU.root");
+  ta_mumudB.Eff();
+  ta_mumuMCB.Eff();
+  makeFullOutput(ta_mumudB, ta_mumuMCB, "RunBOR", "Run B,  Mu17TkMu8 OR Mu17Mu8", 0.01);
 
   //Run C
 
-  ta_mumuRunC=ta_mumud;
-  ta_mumuRunC.setChain(datachainRunC);
-  ta_mumuRunC.setRunCutLow(198022);
-  ta_mumuRunC.setRunCutHigh(203746);
+  ta_mumudC=ta_mumud;
+  ta_mumudC.setChain(datachainRunC);
+  ta_mumudC.setRunCutLow(198022);
+  ta_mumudC.setRunCutHigh(203746);
 
+  ta_mumuMCC=ta_mumuMC;
   ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunCComp.json.txt_PU.root");
-  ta_mumuRunC.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunC, ta_mumuMC, "RunCOR", "Run C,  Mu17TkMu8 OR Mu17Mu8", 0.01);
+  ta_mumudC.Eff();
+  ta_mumuMCC.Eff();
+  makeFullOutput(ta_mumudC, ta_mumuMCC, "RunCOR", "Run C,  Mu17TkMu8 OR Mu17Mu8", 0.01);
 
 
   //Run D
 
-  ta_mumuRunD=ta_mumud;
-  ta_mumuRunD.setChain(datachainRunD);
-  ta_mumuRunD.setRunCutLow(203768);
-  ta_mumuRunD.setRunCutHigh(208686);
+  ta_mumudD=ta_mumud;
+  ta_mumudD.setChain(datachainRunD);
+  ta_mumudD.setRunCutLow(203768);
+  ta_mumudD.setRunCutHigh(208686);
 
-  ta_mumuMC.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
-  ta_mumuRunD.Eff();
-  ta_mumuMC.Eff();
-  makeFullOutput(ta_mumuRunC, ta_mumuMC, "RunDOR", "Run D, Mu17TkMu8 OR Mu17Mu8", 0.01);
+  ta_mumuMCD=ta_mumuMC;
+  ta_mumuMCD.setPUFile(cmssw_base+"/src/TtZAnalysis/Data/RunDprompt.json.txt_PU.root");
+  ta_mumudD.Eff();
+  ta_mumuMCD.Eff();
+  makeFullOutput(ta_mumudC, ta_mumuMCD, "RunDOR", "Run D, Mu17TkMu8 OR Mu17Mu8", 0.01);
 
   
+  ////combine
+
+
+
+  mumutotal=ta_mumudA.getGlobalDen() + ta_mumudB.getGlobalDen() + ta_mumudC.getGlobalDen() + ta_mumudD.getGlobalDen();
+  mumuAscale=mumutotal/ta_mumudA.getGlobalDen() * lumiA/lumitotal;
+  ta_mumudA.scale(mumuAscale);
+  ta_mumuMCA.scale(mumuAscale);
+  mumuBscale=mumutotal/ta_mumudB.getGlobalDen() * lumiB/lumitotal;
+  ta_mumudB.scale(mumuBscale);
+  ta_mumuMCB.scale(mumuBscale);
+  mumuCscale=mumutotal/ta_mumudC.getGlobalDen() * lumiC/lumitotal;
+  ta_mumudC.scale(mumuCscale);
+  ta_mumuMCC.scale(mumuCscale);
+  mumuDscale=mumutotal/ta_mumudD.getGlobalDen() * lumiD/lumitotal;
+  ta_mumudD.scale(mumuDscale);
+  ta_mumuMCD.scale(mumuDscale);
+
+
+  mumudFull = ta_mumudA + ta_mumudB + ta_mumudC + ta_mumudD;
+  mumumcFull = ta_mumuMCA + ta_mumuMCB + ta_mumuMCC + ta_mumuMCD;
+  makeFullOutput(mumudFull, mumumcFull, "FullOR", "Mu17TkMu8 OR Mu17Mu8, 19 fb^{-1}", 0.01);
   
+
+
 }
 
 
