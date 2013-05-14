fad7287b6f8a3ed54679293b85e1a60143842deb
jkiesele <>
2013-05-14 09:35:56 +0000

    check

---
+++ Analysis/checkCPU.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+CPUPROFILE=$1.prof LD_PRELOAD=/afs/desy.de/group/cms/perftool/v2.0/lib/libprofiler.so analyse.exe -c ee -s nominal -o perfout
+
+/afs/desy.de/group/cms/perftool/v2.0/bin/pprof --pdf analyse.exe $1.prof >| $1.pdf
