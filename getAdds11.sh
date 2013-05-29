c0d1e11f53c64c07f7b1f6c905479f9864acad22
jkiesele <>
2013-05-29 10:02:40 +0000

    rm some stuff

--- getAdds11.sh
+++ getAdds11.sh
@@ -11,7 +11,7 @@ addpkg FWCore/GuiBrowsers        V00-00-60
 
 
 echo 'checked out release V08-07-50 - check if newest one'
-echo 'deleting tools not compiling in CMSSW_4_4_X'
+echo 'deleting tools not compiling in CMSSW_4_4_X and not needed for sending jobs'
 
 
 rm -f ${CMSSW_BASE}/src/TtZAnalysis/Tools/src/container*
@@ -20,6 +20,23 @@ rm -f ${CMSSW_BASE}/src/TtZAnalysis/Tools/src/classes.h
 rm -f ${CMSSW_BASE}/src/TtZAnalysis/Tools/src/classes_def.xml
 rm -f ${CMSSW_BASE}/src/TtZAnalysis/Tools/interface/container*
 rm -f ${CMSSW_BASE}/src/TtZAnalysis/Tools/interface/eff*
+
+rm -rf ${CMSSW_BASE}/src/TtZAnalysis/Analysis
+rm -rf ${CMSSW_BASE}/src/TtZAnalysis/Tools
+
+rm -rf ${CMSSW_BASE}/src/TopAnalysis/TopUtils/plugins/*eight*
+rm -rf ${CMSSW_BASE}/src/TopAnalysis/TopUtils/plugins/*vent*
+rm -rf ${CMSSW_BASE}/src/TopAnalysis/TopUtils/plugins/*Seal*
+rm -rf ${CMSSW_BASE}/src/TopAnalysis/TopUtils/python/particleFlowSetup_cff.py
+
+
+rm -rf ${CMSSW_BASE}/src/RecoLocalTracker/SubCollectionProducers
+rm -rf ${CMSSW_BASE}/src/RecoMET
+rm -rf ${CMSSW_BASE}/src/CommonTools/RecoAlgos
+rm -rf ${CMSSW_BASE}/src/DPGAnalysis/Skims
+rm -rf ${CMSSW_BASE}/src/DPGAnalysis/SiStripTools
+rm -rf ${CMSSW_BASE}/src/DataFormats/TrackerCommon
+
 
 rm -f ${CMSSW_BASE}/src/RecoMET/METFilters/plugins/EcalDeadCellBoundaryEnergyFilter.cc
 rm -f ${CMSSW_BASE}/src/RecoMET/METFilters/plugins/HcalLaserEventFilter.cc
