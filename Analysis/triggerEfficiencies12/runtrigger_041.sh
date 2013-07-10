cd49f32ae80cba419a3dc44f08d58d35d4092bd2
Jan <jan.kieseler@desy.de>
2013-07-10 15:47:47 +0200

    for 041

---
+++ Analysis/triggerEfficiencies12/runtrigger_041.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#runs analysis und puts analysis code and output to an extra directory with date flag
+
+dirstring=$1"_041_trigger_$(date +%F_%H:%M)"
+
+echo '\n\nrunning in directory' "${dirstring}."
+
+mkdir $dirstring
+cd $dirstring
+cp ../triggerAnalyzer_base3.h .
+#cp ../prescaleList.h .
+cp ../trigger_041.C .
+cp ../histoStyle.h .
+cp ../getSubSetRoot.C .
+nohup root -l -b -q trigger_041.C++ > output.txt 
+echo "done with ${dirstring}"
