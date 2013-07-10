5f071a0cd7ffdd93b199843a4f8556c3c60eb439
Jan <jan.kieseler@desy.de>
2013-07-10 16:32:47 +0200

    made everything based on same source

---
+++ Analysis/triggerEfficiencies12/runtrigger_028.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#runs analysis und puts analysis code and output to an extra directory with date flag
+
+dirstring=$1"_028_trigger_$(date +%F_%H:%M)"
+
+echo '\n\nrunning in directory' "${dirstring}."
+
+mkdir $dirstring
+cd $dirstring
+cp ../triggerAnalyzer_base3.h .
+#cp ../prescaleList.h .
+cp ../trigger_028.C .
+cp ../histoStyle.h .
+cp ../getSubSetRoot.C .
+nohup root -l -b -q trigger_028.C++ > output.txt 
+echo "done with ${dirstring}"
