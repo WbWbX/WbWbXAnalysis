ea08979cd9b6df88fe303b196f866de2aa2132b1
jkiesele <jan.kieseler@desy.de>
2013-10-17 15:47:55 +0200

    a new tool to make fast plots

--- Analysis/make.sh
+++ Analysis/make.sh
@@ -9,10 +9,10 @@ ROOTLIBS=`root-config --libs`
 CMSLIBS=$CMSSW_BASE/lib/$SCRAM_ARCH/
 #declare -a libs
 libs=("TopAnalysisZTopUtils" 
-"TtZAnalysisDataFormats" 
-"TtZAnalysisTools"
-"TtZAnalysisAnalysis"
-"FWCoreFWLite"
+    "TtZAnalysisDataFormats" 
+    "TtZAnalysisTools"
+    "TtZAnalysisAnalysis"
+    "FWCoreFWLite"
 #"DataFormatsStdDictionaries"
 #"CondFormatsJetMETObjects"
 );
@@ -23,57 +23,68 @@ libs=("TopAnalysisZTopUtils"
 
 if [ $1 == "all" ];
 then
-	
-scram b -j12
+    
+    scram b -j12
 
-libdir=$CMSSW_BASE/src/TtZAnalysis/Analysis/lib
+fi
+if  [ $1 == "all" ] || [ $1 == "run" ];
+then
 
-mkdir -p $libdir
-linklibs=""
+    libdir=$CMSSW_BASE/src/TtZAnalysis/Analysis/lib
 
-for (( i=0;i<${#libs[@]};i++)); do
-linklibs="$linklibs -l${libs[${i}]}"
-cp ${CMSLIBS}lib${libs[${i}]}.so $libdir
-done
+    mkdir -p $libdir
+    linklibs=""
 
-
-echo compiling analyse.C
-infile=analyse.C
-
-g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
-g++ -o analyse.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs -l${CMSSW_BASE}/src/TtZAnalysis/Tools/TUnfold/libunfold.so $infile.o
-
-infile=mergeSyst.cc
-
-echo compiling mergeSyst.cc
-
-g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
-g++ -o mergeSyst.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
+    for (( i=0;i<${#libs[@]};i++)); do
+	linklibs="$linklibs -l${libs[${i}]}"
+	cp ${CMSLIBS}lib${libs[${i}]}.so $libdir
+    done
 
 
-infile=unfold.C
+    echo compiling analyse.C
+    infile=analyse.C
 
-g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
-g++ -o unfold.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
+    g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
+    g++ -o analyse.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs -l${CMSSW_BASE}/src/TtZAnalysis/Tools/TUnfold/libunfold.so $infile.o
+
+    infile=mergeSyst.cc
+
+    echo compiling mergeSyst.cc
+
+    g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
+    g++ -o mergeSyst.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
+
+
+    infile=unfoldTtBar.C
+
+    g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
+    g++ -o unfoldTtBar.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
 fi
 
 if [ $1 == "unfold" ];
 then
-	
+    
 
-libdir=$CMSSW_BASE/src/TtZAnalysis/Analysis/lib
+    libdir=$CMSSW_BASE/src/TtZAnalysis/Analysis/lib
 
-mkdir -p $libdir
-linklibs=""
+    mkdir -p $libdir
+    linklibs=""
 
-for (( i=0;i<${#libs[@]};i++)); do
-linklibs="$linklibs -l${libs[${i}]}"
-done
-	
-	
-infile=unfold.C
+    for (( i=0;i<${#libs[@]};i++)); do
+	linklibs="$linklibs -l${libs[${i}]}"
+    done
+    
+    
+    infile=unfoldTtBar.C
 
-g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
-g++ -o unfold.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
+    g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
+    g++ -o unfoldTtBar.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
 
 fi
+
+
+infile=makePlots.cc
+
+    g++ $ROOTFLAGS -fopenmp -I$CPLUS_INCLUDE_PATH -c -o $infile.o $infile
+    g++ -o makePlots.exe -fopenmp -Wall $ROOTLIBS -Llib$linklibs $infile.o
+
