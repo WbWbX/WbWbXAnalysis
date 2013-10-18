4404defc7db6557fa9a8f42ab186cdbbe891b9fe
Jan <jan.kieseler@desy.de>
2013-10-18 12:39:39 +0200

    fixed small error message when entry was empty

--- Tools/src/fileReader.cc
+++ Tools/src/fileReader.cc
@@ -8,7 +8,13 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
+
 namespace ztop{
+
+
+bool fileReader::debug=false;
+
 std::string & fileReader::ltrim(std::string & str) const{
 	size_t endpos = str.find_last_not_of(trim_);
 	if( std::string::npos != endpos )
@@ -49,29 +55,72 @@ void fileReader::readFile(const std::string &filename){
 	ifstream myfile (filename.data());
 	if (myfile.is_open())
 	{
+		if(debug){
+			std::cout << "fileReader::readFile: opened file.\n searching for start marker " << start_
+					<<" until end marker " << end_ << "\n"
+					<< "delimiter: " << delimiter_ << " comments: " << comment_ << std::endl;
+
+		}
+		bool started=false;
+		bool ended=false;
 		while ( myfile.good() )
 		{
+			if(ended)
+				break;
 			string s;
 
 			if (!getline( myfile, s )) break;
 			trimcomments(s);
 			trim(s);
+
+
 			if(s.size()<1)
 				continue;
+			if(!started && start_.size() !=0 && s.find(start_) == string::npos) //wait for start marker on line basis
+				continue;
+
+			if(debug && !started)
+				std::cout << "found start marker in line" << std::endl;
+
+			//if(end_.size() !=0 && s.compare(end_) == 0) //wait for start marker
+			//break;
+
 			istringstream ss( s );
 			vector <string> record;
-
+			bool noentry=true;
 			while (ss)
 			{
+
 				string s2;
 				if (!getline( ss, s2, *delimiter_.data() )) break;
+				if(!started && start_.size() !=0 && s2 != start_){ //wait for start marker on word by word basis
+					if(debug)
+						std::cout << s2 << " not yet startmarker" << std::endl;
+					continue;
+				}
+				else if(!started){
+					started=true;
+					if(debug)
+						std::cout << s2 << " is start marker, begin reading" << std::endl;
+					continue; //dont read start marker itself
+				}
+				if(end_.size() >0 && s2 == end_){
+					if(debug)
+						std::cout << s2 << " is end marker, stop reading" << std::endl;
+					ended=true;
+					break;
+				}
+
 				trim(s2);
 				if(s2.size()<1)
 					continue;
+				noentry=false;
 				record.push_back( s2 );
 			}
-			lines_.push_back(record);
-			record.clear();
+			if(!ended && !noentry){
+				lines_.push_back(record);
+				record.clear();
+			}
 		}
 
 		myfile.close();
@@ -87,5 +136,41 @@ std::vector<std::string> fileReader::getData(const size_t &line) const{
 		return std::vector<std::string>();
 	}
 	return lines_.at(line);
+}
+/**
+ * if file has entries like somevariable=blabla
+ * getValue("somevariable") will return "blabla"
+ * if value is definied several times, an exception is thrown
+ * less performance but safer
+ * can be switched off by bool
+ * then last value is returned
+ */
+std::string fileReader::getValueString(const std::string & val, bool checkdoubles){
+	std::string out;
+	size_t count=0;
+	//search all entries:
+	for(size_t line=0;line<nLines();line++){
+		for(size_t entr=0;entr<nEntries(line);entr++){
+			std::string & s=lines_.at(line).at(entr);
+			std::size_t pos=s.find(val);
+			if(pos != std::string::npos){ //found
+				if(checkdoubles && count > 0){//except
+					throw std::logic_error("fileReader::getValue: value defined twice! Source of errors!");
+				}
+				pos=s.find("=");
+				if(pos!= std::string::npos){
+					if(pos+1 >= s.size())
+						throw std::logic_error("fileReader::getValue: value definition empty");
+					std::string s2(s.begin()+pos+1,s.end());
+					trim(s2);
+					if(s2.size()<1)
+						throw std::logic_error("fileReader::getValue: value definition empty");
+					out=s2;
+					count++;
+				}
+			}
+		}
+	}
+	return out;
 }
 }//ztop
