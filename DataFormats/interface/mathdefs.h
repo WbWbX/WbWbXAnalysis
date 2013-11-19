1fab998b7bbafc7125b151bd5a8b8a113fd2f6b7
Jan <jan.kieseler@desy.de>
2013-11-19 14:34:19 +0100

    added pi

--- DataFormats/interface/mathdefs.h
+++ DataFormats/interface/mathdefs.h
@@ -4,6 +4,7 @@
 //#include "Math/GenVector/LorentzVector.h"
 //#include "DataFormats/Math/interface/LorentzVector.h"
 
+#define M_PI    3.14159265358979323846f
 
 #include <Math/LorentzVector.h>
 #include <Math/PtEtaPhiM4D.h>
