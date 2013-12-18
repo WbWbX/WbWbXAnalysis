4c3d463d7a8cf6f0236dbf0afdf3cb8c495a2b11
Jan <jan.kieseler@desy.de>
2013-12-18 16:10:04 +0100

    aadded to classes

---
+++ Analysis/src/classes.h
@@ -0,0 +1,13 @@
+#include "../interface/scalefactors.h"
+#include "../interface/reweightfunctions.h"
+
+namespace
+{
+struct dictionary {
+
+    ztop::scalefactors a;
+    ztop::reweightfunctions b;
+
+};
+}
+
