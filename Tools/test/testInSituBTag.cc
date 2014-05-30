13a41bf29fcb930bb7b8ac9ff807c8fa90b4adef
dolinska <ganna.dolinska@cern.ch>
2014-05-30 10:59:42 +0200

    Added insitu cross section calculation

--- Tools/test/testInSituBTag.cc
+++ Tools/test/testInSituBTag.cc
@@ -37,16 +37,17 @@ int main(){
 
     using namespace ztop;
 
-    TString infilelocation="/nfs/dust/cms/user/kiesej/testtemp/emu_8TeV_172.5_nominal_syst.root";
-    TString btagmultiplot="selected b jet multi step 7"; //we use step 7 here, one ONE jet is required
-    TString jetmultiplot="hard jet multi step 7";
+   // TString infilelocation="/nfs/dust/cms/user/kiesej/testtemp/emu_8TeV_172.5_nominal_syst.root";
+    TString infilelocation="/nfs/dust/cms/user/dolinska/inclusive/CMSSW_5_3_14/src/TtZAnalysis/Analysis/output/emu_8TeV_172.5_nominal_pseudodata230.root";
+    TString btagmultiplot="selected b jet multi step 4"; //we use step 7 here, one ONE jet is required
+    TString jetmultiplot="hard jet multi step 4";
 
     /*
      * prepare the input
      */
 
     containerStackVector csv_in;
-    csv_in.loadFromTFile(infilelocation,"emu_8TeV_172.5_nominal_syst");
+    csv_in.loadFromTFile(infilelocation,"emu_8TeV_172.5_nominal_pseudodata230");//_syst");
 
     containerStack bjetmultistack=csv_in.getStack(btagmultiplot);
     containerStack jetmultistack =csv_in.getStack(jetmultiplot);
@@ -56,9 +57,17 @@ int main(){
 
     container1D background=bjetmultistack.getBackgroundContainer(); //this might come handy at some point
     container1D signal=bjetmultistack.getSignalContainer(); //this might come handy at some point
-    container1D data=bjetmultistack.getContainer("data");
+    container1D data=bjetmultistack.getContribution("data");
     container1D dyll=bjetmultistack.getContainer("DY#rightarrowll");
 
+    // -----> Epsilon_emu
+    containerStack genstack = csv_in.getStack("generated events");
+    container1D signalgen = genstack.getSignalContainer();
+    size_t genevtbin=signalgen.getBinNo(1);
+    float genttbarevents=signalgen.getBinContent(genevtbin);
+
+    float eps_emu=signal.integral(true)/genttbarevents; 
+    //-------> end epsilon_emu
 
 
     //which systematics are available??
@@ -70,12 +79,22 @@ int main(){
     container1D onlysystemtics= signal.getSystContainer(somesystindex);
     container1D onlynominal=signal.getSystContainer(-1); // -1 is the index of nominal
 
-    //get a bin
+    // ------> get a bin
     size_t onejetbin=signal.getBinNo(1);
-    float nominal1jetcontentsignal = signal.getBinContent(onejetbin);
-    float somesyst1jetcontentsignal = signal.getBinContent(onejetbin,3); //here: sys layer 3
-    std::cout << "\n" << signal.getSystErrorName(3) <<std::endl; //this one we just selected
+    size_t twojetbin=signal.getBinNo(2);
+    float N1_tt = signal.getBinContent(onejetbin);
+    float N2_tt = signal.getBinContent(twojetbin);
 
+    size_t onejetbinBg=background.getBinNo(1);
+    size_t twojetbinBg=background.getBinNo(2);
+    float nominal1jetcontentbackground = background.getBinContent(onejetbin);
+    float nominal2jetcontentbackground = background.getBinContent(twojetbin);
+
+    size_t onejetbinData=data.getBinNo(1);
+    size_t twojetbinData=data.getBinNo(2);
+    float nominal1jetcontentdata = data.getBinContent(onejetbinData);
+    float nominal2jetcontentdata = data.getBinContent(twojetbinData);
+    // -------> end get bin
 
     // if you want to look at ratios, you might consider using the built-in
     // operators that account for systematics
@@ -85,5 +104,22 @@ int main(){
     // there is already a lot of built-in functionality
 
 
+    ///////////////// CROSS SECTION /////////////////////
+    float L = 19.741; //fb^-1
+
+    float N1_ = nominal1jetcontentdata - nominal1jetcontentbackground;
+    float N2_ = nominal2jetcontentdata - nominal2jetcontentbackground;
+
+    float Ntt_emu = signal.integral(true);
+
+    float Cb = 4*Ntt_emu*N2_tt/(N1_tt+2*N2_tt)/(N1_tt+2*N2_tt);
+
+    float Cross_Section;
+    Cross_Section = (N1_ + 2*N2_)*(N1_ + 2*N2_)*Cb/4/L/eps_emu/N2_;
+    /////////END CROSS SECTION///////////////////////////
+
+
+    std::cout << "Cross section = " << Cross_Section/1000.0 << " pb" << std::endl;
+    std::cout << std::endl;
     std::cout << "DONE" <<std::endl;
 }
