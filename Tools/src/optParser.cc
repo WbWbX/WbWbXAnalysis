03039116af49622b2831ac95cd5e30c33936b1c9
dolinska <ganna.dolinska@cern.ch>
2014-05-30 10:57:49 +0200

    fixed help messages

--- Tools/src/optParser.cc
+++ Tools/src/optParser.cc
@@ -23,13 +23,15 @@ void optParser::doneParsing(){
 
 void optParser::coutHelp()const{
     using namespace std;
-
+    
     std::cout << progname_ << "\n\n" << adddescr_ << "\n usage: "<<std::endl;
     for(size_t i=0;i<help_.size();i++){
-        if(help_.at(i).Length()>0){
+        if(help_.at(i).Length()>0 && !help_.at(i).Contains("display this help message")){
             std::cout << "   " << help_.at(i) << std::endl;
         }
     }
+    cout << "   -h  	display this help message" << endl;
+    cout << "   --help  	display this help message" << endl;
     std::cout << std::endl;
     exit(EXIT_SUCCESS);
 
