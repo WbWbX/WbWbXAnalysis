bbc7108666d79036f87fd29b9067094a209d134d
jkiesele <jan.kieseler@desy.de>
2014-07-23 10:41:49 +0200

    some additional checks

--- prepareEnv.sh
+++ prepareEnv.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/zsh
 
-ln -s ${CMSSW_BASE}/src/TtZAnalysis/Tools/TUnfold/libunfold.so ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so
+mkdir -p ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib
+
+if [[ -a ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so && -a ../TopAnalysis/ZTopUtils/BuildFile.xml ]]
+then
+    echo "CMSSW release already prepared for TtZAnalysis Framework"
+else
+    if [[ ! -a ../TopAnalysis/ZTopUtils/BuildFile.xml ]]
+    then
+	echo "TtZAnalysis depends on the TopAnalysis/ZTopUtils package. Please check it out by hand"
+	exit 1
+    elif  [[ ! -a ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so ]]
+    then
+	ln -s ${CMSSW_BASE}/src/TtZAnalysis/Tools/TUnfold/libunfold.so ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so
+	echo "library symlink set"
+
+    fi
+fi
