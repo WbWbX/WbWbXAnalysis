f46d24e17c48f3032afe8637a0ccc599ac9eb831
Jan <jan.kieseler@desy.de>
2014-07-24 17:46:24 +0200

    some additional checks

--- prepareEnv.sh
+++ prepareEnv.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 mkdir -p ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib
+cd ${CMSSW_BASE}/src/TtZAnalysis
 
-if [[ -a ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so && -a ../TopAnalysis/ZTopUtils/BuildFile.xml ]]
+if [[ -a ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so && -a ../TopAnalysis/ZTopUtils/BuildFile.xml &&  -a ../FWCore/FWLite/BuildFile.xml ]]
 then
     echo "CMSSW release already prepared for TtZAnalysis Framework"
 else
@@ -14,6 +15,10 @@ else
     then
 	ln -s ${CMSSW_BASE}/src/TtZAnalysis/Tools/TUnfold/libunfold.so ${CMSSW_BASE}/external/${SCRAM_ARCH}/lib/libunfold.so
 	echo "library symlink set"
-
+    elif [[ !  -a ../FWCore/FWLite/BuildFile.xml  ]]
+    then
+	mkdir -p ../FWCore
+	ln -s $CMSSW_RELEASE_BASE/src/FWCore/FWLite ../FWCore/FWLite
+	echo "set up link to  FWCore/FWLite"
     fi
 fi
