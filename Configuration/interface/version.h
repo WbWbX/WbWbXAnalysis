8b9d07e30fe9f4017471e6158e320281b8578dc5
jkiesele <jan.kieseler@desy.de>
2015-01-13 15:02:06 +0100

    IMPORTANT! DEFINE TO SWITCH BETWEEN CMSSW5 (gcc 4.6.X) and CMSSW7 (gcc > 4.6.X)

---
+++ Configuration/interface/version.h
@@ -0,0 +1,17 @@
+/*
+ * version.h
+ *
+ *  Created on: Jan 13, 2015
+ *      Author: kiesej
+ */
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+
+#define CMSSW_LEQ_5
+#if __GNUC__ && __GNUC__ > 3 && __GNUC_MINOR__ && __GNUC_MINOR__ > 6
+#undef CMSSW_LEQ_5
+#endif
+
+#endif /* VERSION_H_ */
