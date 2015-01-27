928ee4d636524f6015854e604155f00dd1b6ad9f
Jan <jan.kieseler@desy.de>
2015-01-27 14:26:30 +0100

    MAJOR STRUCTURAL CHANGE

---
+++ DataFormats/test/testDauMoth.cc
@@ -0,0 +1,120 @@
+/*
+ * testDauMoth.cc
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: kiesej
+ */
+
+
+
+
+#include "../interface/dataFormatHelpers.h"
+#include "../interface/NTGenParticle.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TtZAnalysis/Analysis/interface/tBranchHandler.h"
+#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+
+
+/**
+ * Fill in a daughter collection not whole b-collection
+ * Makes use of the fact that inv mass of b's from gluon splitting is 0
+ */
+ztop::NTGenParticle * pickDirectDecayB(const std::vector<ztop::NTGenParticle*> inbs){
+	using namespace ztop;
+	ztop::NTGenParticle * out=0;
+	std::vector<size_t> exclude;
+	for(size_t i=0;i<inbs.size();i++){
+		bool right=true;
+		out=inbs.at(i);
+		float mass;
+		for(size_t j=i;j<inbs.size();j++){
+			if(i==j) continue;
+			const NTLorentzVector<float>& p1= inbs.at(i)->p4();
+			const NTLorentzVector<float>& p2= inbs.at(j)->p4();
+			mass=(p1+p2).m();
+			if(mass < 9.7){ //two bs.. i!=j does same trick
+				right=false;
+				//break;
+			}
+			std::cout << "mass: ("<<i <<","<<j<<") "<< mass << std::endl;
+		}
+		std::cout << "Energie ("<<inbs.at(i)->genId() <<")" << inbs.at(i)->e()<< " mother: " << inbs.at(i)->mothers().at(0)->e() <<std::endl;
+
+	}
+	return out;
+}
+
+int main(){
+	using namespace ztop;
+	using namespace std;
+	AutoLibraryLoader::enable();
+	TFile * f=new TFile("/nfs/dust/cms/user/kiesej/trees_Oct14/tree_7TeV_emuttbar.root");
+	TTree * t = (TTree*) f->Get("PFTree/PFTree");
+
+
+	tBranchHandler<vector<NTGenParticle> > b_GenTops(t,"NTGenTops");
+	tBranchHandler<vector<NTGenParticle> > b_GenWs(t,"NTGenWs");
+	tBranchHandler<vector<NTGenParticle> > b_Genbs(t,"NTGenBs");
+	tBranchHandler<vector<NTGenParticle> > b_Genbrads(t,"NTGenBsRad");
+	tBranchHandler<vector<NTGenParticle> > b_GenLeptons3(t,"NTGenLeptons3");
+	tBranchHandler<vector<NTGenParticle> > b_GenLeptons1(t,"NTGenLeptons1");
+
+
+
+	for(Long64_t entry=0;entry<5000;entry++){
+		b_GenTops.getEntry(entry);
+		b_GenWs.getEntry(entry);
+		b_Genbs.getEntry(entry);
+		b_Genbrads.getEntry(entry);
+		b_GenLeptons3.getEntry(entry);
+
+
+		std::vector<NTGenParticle*> tops=produceCollection(b_GenTops.content());
+		std::vector<NTGenParticle*> bs=produceCollection(b_Genbs.content(),&tops);
+		std::vector<NTGenParticle*> Ws=produceCollection(b_GenWs.content(),&tops);
+		std::vector<NTGenParticle*> brads=produceCollection(b_Genbrads.content(),&bs);
+		std::vector<NTGenParticle*> Leps=produceCollection(b_GenLeptons3.content(),&Ws);
+		std::vector<NTGenParticle*> Leps1=produceCollection(b_GenLeptons1.content(),&Leps);
+
+		bool coutchain=false;
+
+		for(size_t i=0;i<bs.size();i++){
+			if(bs.at(i)->daughters().size()>1){
+				coutchain=true;
+				//for(size_t j=0;j<bs.at(i)->daughters().size();j++){
+					std::cout << "\nid of direct decay: " << pickDirectDecayB(bs.at(i)->daughters())->genId()
+							<<std::endl;
+				//}
+			}
+		}
+
+		if(coutchain){
+			for(size_t i=0;i<tops.size();i++){
+				NTGenParticle * top=tops.at(i);
+				std::cout << "Top: pdg " << top->pdgId() << " pt: " << top->pt() << " eta: " << top->eta() <<std::endl;
+				for(size_t j=0;j<top->daughters().size();j++){
+					NTGenParticle* dau=top->daughters().at(j);
+					std::cout << "|--- " << dau->pdgId() << " pt " << dau->pt() << " eta " << dau->eta()
+												<<" genid: " << dau->genId()<<" ndau: " << dau->daughters().size()<<std::endl;
+					for(size_t k=0;k<dau->daughters().size();k++){
+						NTGenParticle* daudau=dau->daughters().at(k);
+						std::cout << "     |--- " << daudau->pdgId() << " pt " << daudau->pt() << " eta " << daudau->eta()
+								<<" genid: " << daudau->genId()<<std::endl;
+						for(size_t l=0;l<daudau->daughters().size();l++){
+							NTGenParticle* daudaudau=daudau->daughters().at(l);
+							std::cout << "          |--- " << daudaudau->pdgId() << " pt " << daudaudau->pt() << " eta " << daudaudau->eta()
+													<<" genid: " << daudaudau->genId()<<std::endl;
+						}
+					}
+				}
+				std::cout <<std::endl;
+
+			}
+		}
+
+	}
+
+
+	return 0;
+}
---
+++ Tools/test/2Dchi2.cc
@@ -0,0 +1,75 @@
+/*
+ * 2Dchi2.cc
+ *
+ *  Created on: Jan 20, 2015
+ *      Author: kiesej
+ */
+
+
+
+
+#include "../interface/containerStackVector.h"
+#include "TopAnalysis/ZTopUtils/interface/miscUtils.h"
+#include "../interface/plotter2D.h"
+
+#include "TtZAnalysis/Analysis/app_src/mainMacro.h"
+#include "TString.h"
+#include "TStyle.h"
+
+invokeApplication(){
+	using namespace ztop;
+	const std::vector<TString> files = parser->getRest<TString>();
+
+	if(files.size()!=2)
+		return 1;
+
+	containerStackVector csv;
+	csv.loadFromTFile(files.at(0));
+	containerStack stack=csv.getStack("m_lb min step 8");
+	container1DUnfold cuf1=stack.getSignalContainer1DUnfold();
+
+	csv.loadFromTFile(files.at(1));
+	stack=csv.getStack("m_lb min step 8");
+	container1DUnfold cuf2=stack.getSignalContainer1DUnfold();
+
+	std::vector<float> newbins;
+	newbins << 0   << 75 << 100 << 125  << 200 << 350;
+
+	cuf1=cuf1.rebinToBinning(newbins);
+	cuf2=cuf2.rebinToBinning(newbins);
+
+	container2D resp1=cuf1.getNormResponseMatrix();
+	container2D resp2=cuf2.getNormResponseMatrix();
+
+	container2D diff=resp1-resp2;
+
+	gStyle->SetOptStat(0);
+
+	TCanvas c;
+	plotter2D pl;
+	pl.setPlot(&resp1,false);
+	pl.printToPdf((files.at(0)+"_resp").Data());
+
+	pl.cleanMem();
+	pl.setPlot(&resp2,false);
+	pl.printToPdf((files.at(1)+"_resp").Data());
+
+	pl.cleanMem();
+	pl.setPlot(&diff,false);
+	pl.printToPdf((files.at(0)+"_"+ files.at(1) +"_diff").Data());
+
+	size_t ndof;
+	container2D chi2=resp1.chi2container(resp2,&ndof);
+
+
+	pl.cleanMem();
+	pl.setPlot(&chi2,false);
+	pl.printToPdf((files.at(0)+"_"+ files.at(1) +"_chi2").Data());
+
+
+	float gchi2=resp1.chi2(resp2);
+
+	std::cout << "chi2: " << gchi2 << " ndof: " << ndof <<std::endl;
+
+	return 0;
+}
