ba6068714ab11a5e137d332a22762afd1d1577d6
jkiesele <jan.kieseler@desy.de>
2015-01-28 11:27:37 +0100

    forgotten file

---
+++ Analysis/interface/analysisPlotsTtbarXsecFit.h
@@ -0,0 +1,50 @@
+/*
+ * analysisPlotsJan.h
+ *
+ *  Created on: Feb 12, 2014
+ *      Author: kiesej
+ */
+
+#ifndef ANALYSISPLOTSTTXSECFIT_H_
+#define ANALYSISPLOTSTTXSECFIT_H_
+
+#include "analysisPlots.h"
+
+namespace ztop{
+/**
+ * This is a misuse of the container1DUnfold class
+ */
+class analysisPlotsTtbarXsecFit : public analysisPlots{
+public:
+	analysisPlotsTtbarXsecFit(size_t step):analysisPlots(step),jetcategory(cat_0bjet0jet)
+	{
+		leadjetpt_plots.resize(cat_bjetjetmax,0);
+		secondjetpt_plots.resize(cat_bjetjetmax,0);
+		thirdjetpt_plots.resize(cat_bjetjetmax,0);
+		total_plots.resize(cat_bjetjetmax,0);
+	}
+
+	~analysisPlotsTtbarXsecFit(){/* destruction is handled in base class! */}
+
+	void bookPlots();
+	void fillPlotsReco();
+	void fillPlotsGen();
+
+private:
+	enum bjetnjetscategories{
+		cat_0bjet0jet,cat_0bjet1jet,cat_0bjet2jet,cat_0bjet3jet,
+		cat_1bjet0jet,cat_1bjet1jet,cat_1bjet2jet,cat_1bjet3jet,
+		cat_2bjet0jet,cat_2bjet1jet,cat_2bjet2jet,cat_2bjet3jet,cat_bjetjetmax}
+	jetcategory;
+
+	void setJetCategory(size_t nbjets,size_t njets);
+
+
+
+	std::vector<container1DUnfold*>
+	leadjetpt_plots, secondjetpt_plots, thirdjetpt_plots, total_plots;
+
+};
+
+}
+#endif /* analysisPlotsTtbarXsecFit_H_ */
